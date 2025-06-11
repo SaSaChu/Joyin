2c32f926af307ae8a99fea2e4dc30ce41ee42a03
sasachu <teresa7397@gmail.com>
2025-06-11 18:23:27 +0800

    初架構

--- src/js/public.js
+++ src/js/public.js
@@ -1,9 +1,17 @@
-$(function () {
-  $(window).on("scroll", function () {
-    if ($(this).scrollTop() > 100) {
-      $("header").addClass("scrolled");
-    } else {
-      $("header").removeClass("scrolled");
-    }
+$(document).ready(function () {
+  // 範例：點擊 email 自動複製
+  $('.footer-section a[href^="mailto"]').click(function (e) {
+    e.preventDefault();
+    const email = $(this).text();
+    navigator.clipboard.writeText(email).then(() => {
+      alert("Email 已複製到剪貼簿");
+    });
+  });
+
+  // 表單
+  $("#contactForm").on("submit", function (e) {
+    e.preventDefault();
+    alert("感謝您的填寫，我們會盡快與您聯繫！");
+    this.reset();
   });
 });
