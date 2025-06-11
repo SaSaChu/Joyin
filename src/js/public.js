a2e6db3c925491eff64fdef25e0df55e6cbffc50
sasachu <teresa7397@gmail.com>
2025-06-11 17:11:47 +0800

    no message

--- src/js/public.js
+++ src/js/public.js
@@ -1,6 +1,6 @@
 $(function () {
   $(window).on("scroll", function () {
-    if ($(this).scrollTop() > 50) {
+    if ($(this).scrollTop() > 100) {
       $("header").addClass("scrolled");
     } else {
       $("header").removeClass("scrolled");
